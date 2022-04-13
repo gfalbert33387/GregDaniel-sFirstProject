73897bb808660055c7f30a172f064711f64b5d71
gfalbert33387 <gregsite.mythlore@gmail.com>
2022-04-13 00:00:45 -0400

    These files might be buried in a weird way, but don't move them around or they might break.

---
+++ DoodlingHumansProject1/src/Actors/Player.gd
@@ -0,0 +1,14 @@
+extends Actor
+
+
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _physics_process(delta: float) -> void:
+	pass
+	
+	# if is_rolling == false: calc_velocity_normal()
+
+func calc_velocity_normal(old_velocity: Vector2) -> void:
+	pass
